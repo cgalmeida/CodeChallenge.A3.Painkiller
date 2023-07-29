f1371f4010b309bdc36f4ef83e2cc06c90e662f8
cgalmeida <carolinagalmeida.eng@gmail.com>
2023-07-29 17:13:27 -0300

    ✨(feat):setup-migrations

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UUID
 from app.db.base import Base
+import uuid
 
-class Category(Base)
+class Patient(Base):
+    __tablename__ = 'patients'
+    id = Column('patient_id', UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    first_name = Column('first_name', String, nullable=False)
+    last_name = Column('last_name', String, nullable=False)
+    age = Column('age', Integer, nullable=False)
+    condition = Column('condition', String, nullable=True)
